d01fe7b131a4be175264398e64923eabc58802a5
邹上豪 <zoush@kean.edu>
2023-02-13 18:02:24 +0800

    *
    签到系统的理论测试基本结束，接下来开始branch实际测试

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,23 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 //此类用于各种疑难理论测试，成功后可使用branch功能进行实际测试
 //现在正在测试：签到功能
 /*
- * 原理：每次签到获取当前的日期及时间，若上次签到的年月日与这次的一致，则直接判定签到取失败；若不一致，则接着与上次签到的年月日进行对比，如果两次签到日期相差大于或等于1天，则这次签到成功。
+ * 原理：每次签到获取当前的年月日，若上次签到的年月日与这次的一致，则直接判定签到失败；若不一致，则接着与上次签到的年月日进行对比，如果两次签到日期相差大于或等于1天，则这次签到成功。
  * 如果两次签到日期相差刚好为1天，则判定为连续签到，获得更多积分奖励。若两次签到日期相差大于1天，则连续签到天数重置为0。
+ *
+ * 增加签到功能后，User类可能会增加以下数据：积分数，上次签到时间，等级，连续签到天数
  * */
 public class Test {
-    static LocalDateTime lastDateTime = LocalDateTime.of(2020, 3, 24, 0, 0, 0);
+    //默认的上次签到时间，用于用户第一次签到
+    static LocalDateTime lastLDT = LocalDateTime.of(2020, 3, 24, 0, 0, 0);
+    //连续签到天数
     static int succession = 0;
+    //单次签到获得的积分数
     static int point = 1;
+    //总积分数
     static int sumPoint = 0;
 
     public static void main(String[] args) {
@@ -22,20 +28,19 @@ public class Test {
             str = input.next();
             if (str.equals("1")) {
                 if (signIn())
-                    System.out.printf("签到成功，已连续签到%d天，获得%d积分，目前积分%d\n", succession, point, sumPoint);
-                else System.out.println("今天已经签到过了");
+                    System.out.printf("签到成功，已连续签到%d天，获得%d积分，目前积分%d，目前等级%d\n", succession, point, sumPoint, level(sumPoint));
+                else System.out.println("今天已经签到过了，明天再来吧！");
             }
         }
     }
 
+    ///签到
     private static boolean signIn() {
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        DateTimeFormatter formatToDate = DateTimeFormatter.ofPattern("dd");
-        int current = Integer.parseInt(currentDateTime.format(formatToDate));
-        int last = Integer.parseInt(lastDateTime.format(formatToDate));
-        if (current == last) {
+        LocalDateTime currentLDT = LocalDateTime.now();
+        long span = ChronoUnit.DAYS.between(lastLDT, currentLDT);
+        if (span == 0) {
             return false;
-        } else if (current - last == 1) {
+        } else if (span == 1) {
             point += succession;
             succession++;
         } else {
@@ -43,7 +48,12 @@ public class Test {
             succession = 1;
         }
         sumPoint += point;
-        lastDateTime = lastDateTime.with(currentDateTime);
+        lastLDT = lastLDT.with(currentLDT);
         return true;
+    }
+
+    ///等级
+    private static int level(int sumPoint) {
+        return 1 + sumPoint / 100;
     }
 }
