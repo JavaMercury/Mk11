c62e5e452d4f261c2c08a443a88c9917a7fba75a
邹上豪 <zoush@kean.edu>
2023-03-07 12:20:32 +0800

    *

--- src/Initializer.java
+++ src/Initializer.java
@@ -23,7 +23,7 @@ public abstract class Initializer extends JFrame implements KeyListener, MouseLi
     }
 
     JMenu aboutJM = new JMenu("关于(G)");
-    String version = "水银第11代 0.11.08.20230222";
+    String version = "水银第11代 0.11.09.20230307";
     String username;
     String password;
     JDialog aboutJD = new JDialog();
